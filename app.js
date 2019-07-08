d073161c560771590dd753e1ec8c70bcec2764e8
Vicky Sharma <vicky.sharma@digitbazar.com>
2019-07-08 12:40:10 +0530

    Project setup for graphql and mongodb

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const mongoose = require("mongoose");
+
+const graphQlSchema = require("./graphql/schema/index");
+const graphQlResolvers = require("./graphql/resolvers/index");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
+    graphiql: true
+  })
+);
+
+mongoose
+  .connect(process.env.MONGO_DB, { useNewUrlParser: true })
+  .then(() => {
+    app.listen(3000, () => console.log("Server Started"));
+  })
+  .catch(err => {
+    console.log(err);
+  });
